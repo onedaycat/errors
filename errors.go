55468940e3152018b35580cf9bdec62817ca1cd5
witooh <witoo@plimble.com>
2019-03-08 06:13:07 +0700

    update

--- errors.go
+++ errors.go
@@ -72,11 +72,6 @@ type Error interface {
 	WithUnknownError() *AppError
 	Format(s fmt.State, verb rune)
 	StackStrings() []string
-	IsNotFound() bool
-	IsInternalError() bool
-	IsBadRequest() bool
-	IsUnauthorized() bool
-	IsForbidden() bool
 
 	GetStatus() int
 	GetCode() string
@@ -241,26 +236,6 @@ func (e *AppError) WithUnknownError() *AppError {
 func (e *AppError) WithInput(input interface{}) *AppError {
 	e.Input = input
 	return e
-}
-
-func (e *AppError) IsNotFound() bool {
-	return e.Status == NotFoundStatus
-}
-
-func (e *AppError) IsInternalError() bool {
-	return e.Status == InternalErrorStatus
-}
-
-func (e *AppError) IsBadRequest() bool {
-	return e.Status == BadRequestStatus
-}
-
-func (e *AppError) IsUnauthorized() bool {
-	return e.Status == UnauthorizedStatus
-}
-
-func (e *AppError) IsForbidden() bool {
-	return e.Status == ForbiddenStatus
 }
 
 func (e *AppError) Format(s fmt.State, verb rune) {
@@ -439,22 +414,22 @@ func ErrStatus(err error) int {
 	return herr.Status
 }
 
-func IsNotFound(err Error) bool {
-	return err != nil && err.GetStatus() == 404
+func IsNotFound(err *AppError) bool {
+	return err != nil && err.Status == 404
 }
 
-func IsInternalError(err Error) bool {
-	return err != nil && err.GetStatus() == 500
+func IsInternalError(err *AppError) bool {
+	return err != nil && err.Status == 500
 }
 
-func IsBadRequest(err Error) bool {
-	return err != nil && err.GetStatus() == 400
+func IsBadRequest(err *AppError) bool {
+	return err != nil && err.Status == 400
 }
 
-func IsUnauthorized(err Error) bool {
-	return err != nil && err.GetStatus() == 401
+func IsUnauthorized(err *AppError) bool {
+	return err != nil && err.Status == 401
 }
 
-func IsForbidden(err Error) bool {
-	return err != nil && err.GetStatus() == 403
+func IsForbidden(err *AppError) bool {
+	return err != nil && err.Status == 403
 }
