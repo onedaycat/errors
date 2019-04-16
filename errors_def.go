4bf1674fdecf8925619910139f9c8ded88f7a549
witooh <witoo@plimble.com>
2019-04-16 22:38:46 +0700

    update

--- errors_def.go
+++ errors_def.go
@@ -9,38 +9,18 @@ type ErrorDefinition struct {
 	Msg    string
 }
 
-func Def(code, errType string) *ErrorDefinition {
+func Def(code string) *ErrorDefinition {
 	e := &ErrorDefinition{
-		Code: code,
-		Type: errType,
-	}
-
-	switch errType {
-	case BadRequestType:
-		e.Status = 400
-	case UnauthorizedType:
-		e.Status = 401
-	case ForbiddenType:
-		e.Status = 403
-	case NotFoundType:
-		e.Status = 404
-	case TimeoutType:
-		e.Status = 441
-	case InternalErrorType:
-		e.Status = 500
-	case NotImplementType:
-		e.Status = 501
-	case UnavailableType:
-		e.Status = 503
-	case UnknownErrorType:
-		e.Status = 520
+		Code:   code,
+		Status: UnknownErrorStatus,
+		Type:   UnknownErrorType,
 	}
 
 	return e
 }
 
-func DefM(code, errType, msg string) *ErrorDefinition {
-	e := Def(code, errType)
+func DefM(code, msg string) *ErrorDefinition {
+	e := Def(code)
 	e.Msg = msg
 
 	return e
@@ -87,4 +67,58 @@ func (e *ErrorDefinition) WithInput(input interface{}) *AppError {
 
 func (e *ErrorDefinition) Is(err Error) bool {
 	return e.Code == err.GetCode()
+}
+
+func (e *ErrorDefinition) BadRequest() *ErrorDefinition {
+	e.Type = BadRequestType
+	e.Status = 400
+	return e
+}
+
+func (e *ErrorDefinition) Unauthorized() *ErrorDefinition {
+	e.Type = UnauthorizedType
+	e.Status = 401
+	return e
+}
+
+func (e *ErrorDefinition) Forbidden() *ErrorDefinition {
+	e.Type = ForbiddenType
+	e.Status = 403
+	return e
+}
+
+func (e *ErrorDefinition) NotFound() *ErrorDefinition {
+	e.Type = NotFoundType
+	e.Status = 404
+	return e
+}
+
+func (e *ErrorDefinition) Timeout() *ErrorDefinition {
+	e.Type = TimeoutType
+	e.Status = 441
+	return e
+}
+
+func (e *ErrorDefinition) InternalError() *ErrorDefinition {
+	e.Type = InternalErrorType
+	e.Status = 500
+	return e
+}
+
+func (e *ErrorDefinition) NotImplement() *ErrorDefinition {
+	e.Type = NotImplementType
+	e.Status = 501
+	return e
+}
+
+func (e *ErrorDefinition) Unavailable() *ErrorDefinition {
+	e.Type = UnavailableType
+	e.Status = 503
+	return e
+}
+
+func (e *ErrorDefinition) UnknownError() *ErrorDefinition {
+	e.Type = UnknownErrorType
+	e.Status = 520
+	return e
 }
